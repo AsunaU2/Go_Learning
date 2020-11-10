1b6ebad248d463cc132c44d16f2b73fce37f3f75
KiritoU2 <kiritou2@outlook.com>
2020-11-10 22:49:30 +0800

    增加for的写法

--- ch5/loop/loop_test.go
+++ ch5/loop/loop_test.go
@@ -2,7 +2,6 @@ package loop_test
 
 import "testing"
 
-
 /*
 // 无线循环的写法：
 n:=0
@@ -13,9 +12,16 @@ for {
 */
 
 func TestWhileLoop(t *testing.T) {
-    n :=0
-    for n != 5 {
-        t.Log(n)
-        n++
-    }
+	n := 0
+	for n != 5 {
+		t.Log(n)
+		n++
+	}
+
+	/*
+	   另外也有和c++一样的写法
+	*/
+	for i := 0; i < 5; i++ {
+		t.Log(i)
+	}
 }
