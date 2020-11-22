c13af234ca3a1127ae6b75de503c9fabffccc3bc
KiritoU2 <kiritou2@outlook.com>
2020-11-22 17:34:07 +0800

    增加注释理解

--- ch11/interface/interface_test.go
+++ ch11/interface/interface_test.go
@@ -17,7 +17,7 @@ type GoProgrammer struct {
 	Language string
 }
 
-// 定义类的方法
+// 定义类的方法 // 为何这样声明呢，答案很简单，让stProg对于WriteHelloWorld这个方法可见，当然了这里我没用到就是了
 func (stProg *GoProgrammer) WriteHelloWorld() string {
 	return "fmt.Println(\"Hello world\")"
 }
