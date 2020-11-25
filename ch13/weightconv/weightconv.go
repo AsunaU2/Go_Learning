524f12cd6bfa102b1e7185ba937f1faf9e9bc490
KiritoU2 <kiritou2@outlook.com>
2020-11-25 21:37:54 +0800

    增加注释

--- ch13/weightconv/weightconv.go
+++ ch13/weightconv/weightconv.go
@@ -2,17 +2,22 @@ package weightconv
 
 import "fmt"
 
+// Kilogram 公斤
 type Kilogram float64
+
+// Pound 磅
 type Pound float64
 
 const (
 	transferUnit float64 = 0.453592
 )
 
+// KToP 公斤转磅
 func KToP(k Kilogram) Pound {
 	return Pound(k / Kilogram(transferUnit))
 }
 
+// PToK 磅转公斤
 func PToK(p Pound) Kilogram {
 	return Kilogram(p * Pound(transferUnit))
 }
